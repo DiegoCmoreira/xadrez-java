71eab025422510cf3122a4a8aef75b4345739725
DiegoCmoreira <diego.moreira@sptech.school>
2023-03-24 12:12:25 -0300

    Implementando os movimentos possíveis do Rei

--- chess_system/src/aplicacao/UI.java
+++ chess_system/src/aplicacao/UI.java
@@ -29,26 +29,23 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
-	 System.out.print("\033[H\033[2J");
-	 System.out.flush();
-	} 
-	
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		}
-		catch(RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Erro ao ler a posição do xadrez. Valores válidos vão de a1 até h8.");
 		}
 	}
-	
-	
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
@@ -70,11 +67,11 @@ public class UI {
 			System.out.println();
 		}
 		System.out.print("  a b c d e f g h");
-	
-	
+	}
+
 	private static void printPiece(ChessPiece piece, boolean background) {
-		if(background) {
-			System.out.print(ANSI_BLUE_BACKGROUND );
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
